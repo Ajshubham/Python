98f61220af336f0faa713b5d520bfd31360f45f5
Shubham Shatabdi <76531082+Ajshubham@users.noreply.github.com>
2022-11-30 23:31:41 +0530

    oops concept in python

---
+++ Oops.py
@@ -0,0 +1,12 @@
+class Test:
+    def __init__(self):
+        self.name = 'Shubham'
+        self.roll = 114
+        self.gpa = 8.6
+    def display(self):
+        print(self.name)
+        print(self.roll)
+        print(self.gpa)
+
+t = Test()
+t.display()
