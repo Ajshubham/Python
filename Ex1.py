9325f6f6c50fca025ccee05c5c9b2b04fd517e5f
Shubham Shatabdi <76531082+Ajshubham@users.noreply.github.com>
2022-12-19 22:23:19 +0530

    Used Dictionary

---
+++ Ex1.py
@@ -0,0 +1,13 @@
+# Write a program that count frequency of each word in text.
+
+text = input('Enter a sentence: ')
+wdict = {}
+words = text.split()
+for word in words:
+    if word in wdict:
+        wdict[word] += 1
+    else:
+        wdict[word] = 1
+
+for key in wdict:
+    print(key, wdict[key])
